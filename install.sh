c57b786452077e3134bc51b2d717454bfbe99e33
hascar <arm.scar@gmail.com>
2021-10-22 16:30:27 +0200

    added bin/bash tag for install sh file

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#
+# verifier.sh  Copyright (c) 2021, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+
+
 BRANCH_ID="$1"
 DB_PASSWORD="$2"
 HOST_SMTP="$3"
