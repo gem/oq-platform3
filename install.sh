18c9c0eabeb75822495122c951488020cb68f71d
Armando Scarpati <arm.scar@gmail.com>
2021-11-12 12:28:03 +0200

    moved -T options for docker-compose exec to exec commands

--- install.sh
+++ install.sh
@@ -83,9 +83,9 @@ sleep 200
 echo "Installation complete."
 
 # Run commands on django container
-docker-compose -T exec django bash -c "./manage.sh create_gem_user"
-docker-compose -T exec django bash -c "./manage.sh add_user /usr/src/openquakeplatform/data_commands/auth_user.json"
-docker-compose -T exec django bash -c "./manage.sh add_documents"
+docker-compose exec -T django bash -c "./manage.sh create_gem_user"
+docker-compose exec -T django bash -c "./manage.sh add_user /usr/src/openquakeplatform/data_commands/auth_user.json"
+docker-compose exec -T django bash -c "./manage.sh add_documents"
 #docker-compose exec django bash -c "./manage.sh loaddata /usr/src/openquakeplatform/data_commands/base_topiccategory.json"
 
 #function complete procedure for tests
