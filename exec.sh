ac4be882f9c8adf312a69f5a4afe119596cdb1a7
Armando Scarpati <arm.scar@gmail.com>
2021-11-29 16:05:03 +0200

    added variable name_project to exec install script

--- exec.sh
+++ exec.sh
@@ -8,6 +8,7 @@
 # copy this file in /home before to exec
 #
 
+export nameproject="$1"
 
 cd $HOME/openquakeplatform
 docker-compose down
@@ -19,4 +20,4 @@ cd $HOME/oq-platform3
 export GEM_GIT_PACKAGE="oq-platform3"
 export GEM_GIT_REPO="git://github.com/gem"
 export NO_EXEC_TEST="notest"
-./install.sh
+./install.sh "" "" "" "" $name_project
