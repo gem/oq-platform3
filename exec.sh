7ed3aa4f1653466ebaed4a0bee9fe3b2903dcff7
Armando Scarpati <arm.scar@gmail.com>
2021-11-29 15:45:30 +0200

    added comments and specification in exec script

--- exec.sh
+++ exec.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#
 # install complete of management of dockers
 # containers and images
+#
+# tests active with customization of NO_EXEC_TEST variable
+#
+# copy this file in /home before to exec
+#
 
 
 cd $HOME/openquakeplatform
@@ -14,4 +20,3 @@ export GEM_GIT_PACKAGE="oq-platform3"
 export GEM_GIT_REPO="git://github.com/gem"
 export NO_EXEC_TEST="notest"
 ./install.sh
-
