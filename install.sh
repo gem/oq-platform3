66ae92598faaf07afff29445f55c36c49cd14491
hascar <arm.scar@gmail.com>
2022-04-29 09:05:04 +0200

    enabled add_user command

--- install.sh
+++ install.sh
@@ -136,7 +136,7 @@ sleep 200
 docker-compose exec -T django bash -c "chmod +x *.sh"
 # docker-compose exec -T db bash -c "/data_commands/gs_data/sql/dump.bash"
 docker-compose exec -T django bash -c "./manage.sh create_gem_user"
-# docker-compose exec -T django bash -c "./manage.sh add_user /usr/src/openquakeplatform/data_commands/auth_user.json"
+docker-compose exec -T django bash -c "./manage.sh add_user /usr/src/openquakeplatform/data_commands/auth_user.json"
 # docker-compose exec -T django bash -c "./manage.sh add_documents"
 # docker-compose exec django bash -c "./manage.sh loaddata /usr/src/openquakeplatform/data_commands/base_topiccategory.json"
 docker-compose exec -T django bash -c "./manage.sh makemigrations"
