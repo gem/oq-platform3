0c6ca48202bc09c3410da400133fc4705117e474
hascar <arm.scar@gmail.com>
2022-05-09 15:36:04 +0200

    inverted migrate commands with create_gem_user command

--- install.sh
+++ install.sh
@@ -148,9 +148,9 @@ sleep 200
 
 # Run commands on django container
 docker-compose exec -T django bash -c "chmod +x *.sh"
-docker-compose exec -T django bash -c "./manage.sh create_gem_user"
 docker-compose exec -T django bash -c "./manage.sh makemigrations"
 docker-compose exec -T django bash -c "./manage.sh migrate"
+docker-compose exec -T django bash -c "./manage.sh create_gem_user"
 # docker-compose exec -T db bash -c "/usr/src/openquakeplatform/data_commands/gs_data/sql/dump.bash"
 docker-compose exec -T django bash -c "./manage.sh add_user /usr/src/openquakeplatform/data_commands/auth_user.json"
 # docker-compose exec -T django bash -c "./manage.sh add_documents"
