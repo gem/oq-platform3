e880daf1505ea2c3237508606b1a00d9213b27eb
hascar <arm.scar@gmail.com>
2022-05-11 17:12:30 +0200

    disabled fixsitename from install script

--- install.sh
+++ install.sh
@@ -150,7 +150,7 @@ sleep 200
 docker-compose exec -T django bash -c "chmod +x *.sh"
 docker-compose exec -T django bash -c "./manage.sh makemigrations"
 docker-compose exec -T django bash -c "./manage.sh migrate"
-docker-compose exec -T django bash -c "./manage.sh fixsitename"
+# docker-compose exec -T django bash -c "./manage.sh fixsitename"
 docker-compose exec -T django bash -c "./manage.sh create_gem_user"
 # docker-compose exec -T db bash -c "/usr/src/openquakeplatform/data_commands/gs_data/sql/dump.bash"
 # docker-compose exec -T django bash -c "./manage.sh add_user /usr/src/openquakeplatform/data_commands/auth_user.json"
